ecf47cf5f8441f13bec8cc3ddda3cc9451adb559
Administrator <Administrator@SKY-20180524SZH.localdomain>
2020-10-03 16:31:26 +0800

    fixed1

--- src/j1/Test.java
+++ src/j1/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] a) {
 		System.out.println("fets");
 		for (int i=0;i<99;i++) {
-			System.out.println("ok");
+			System.out.println("fixed1");
 		}
 	}
 }
