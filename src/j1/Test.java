a15a39862d38b650ad394ed9aabbca30460bd2cb
Administrator <Administrator@SKY-20180524SZH.localdomain>
2020-10-03 16:35:13 +0800

    fix bug3

--- src/j1/Test.java
+++ src/j1/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] a) {
 		System.out.println("fets");
 		for (int i=0;i<99;i++) {
-			System.out.println("fixed1");
+			System.out.println("fixed1");//fixed bug3
 		}
 	}
 }
--- src/j1/Test2.java
+++ src/j1/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] a) {
 		System.out.println("fets");
 		for (int i=0;i<99;i++) {
-			System.out.println("fixed2");
+			System.out.println("fixed2");//fixed bug3
 		}
 	}
 }
